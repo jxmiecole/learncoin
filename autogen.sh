9be1593fe279248977fb881a133cbee7f850a7fa
jxmiecole <jxmiecole@gmail.com>
2021-05-25 22:44:41 +0100

    Fixed permissions

---
+++ autogen.sh
@@ -0,0 +1,16 @@
+#! /bin/sh
+sudo apt-get install git -y
+sudo apt-get update
+sudo apt-get install build-essential libtool autotools-dev automake pkg-config libssl-dev libevent-dev bsdmainutils -y
+sudo apt-get install libboost-system-dev libboost-filesystem-dev libboost-chrono-dev libboost-program-options-dev libboost-test-dev libboost-thread-dev -y
+sudo apt-get install libboost-all-dev -y
+sudo apt-get install software-properties-common -y
+sudo add-apt-repository ppa:bitcoin/bitcoin -y
+sudo apt-get update
+sudo apt-get install libdb4.8-dev libdb4.8++-dev -y
+sudo apt-get install libminiupnpc-dev -y
+sudo apt-get install libzmq3-dev -y
+sudo apt-get install libqt5gui5 libqt5core5a libqt5dbus5 qttools5-dev qttools5-dev-tools libprotobuf-dev protobuf-compiler -y
+sudo apt-get install libqt4-dev libprotobuf-dev protobuf-compiler -y
+sudo apt-get remove libssl-dev -y
+sudo apt install libssl1.0-dev -y
